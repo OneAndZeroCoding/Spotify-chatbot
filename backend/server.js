2782db4a3a80eb2ad5b3d6db9d16371124d08419
OneAndZeroCoding <oneandzerocoding@gmail.com>
2025-09-08 21:20:09 +0530

    Added server.js debugging

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const spotifyRoutes = require('./routes/spotify');
 const convertRoutes = require('./routes/convert');
 const authRoutes = require('./routes/auth');
 const app = express();
+console.log("Initializing middleware...");
 app.use(express.json());
 
 // Use sessions to store user access token
@@ -17,8 +18,11 @@ app.use(session({
 
 // Mount Spotify routes
 app.use('/auth', authRoutes);
+console.log("Mounted /auth routes");
 app.use('/spotify', spotifyRoutes);
+console.log("Mounted /spotfiy routes");
 app.use('/convert', convertRoutes);
+console.log("Mounted /convert routes");
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
